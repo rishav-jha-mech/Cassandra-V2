8467b1a66118ab1cd968fd2de9feb2242a7d66bd
rishav-jha-mech <rishav.rj74@gmail.com>
2021-10-20 23:41:04 +0530

    Modified to serve static files

--- Cassandra/urls.py
+++ Cassandra/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import handler404,handler500
 
 
 urlpatterns = [
     path('RohiniBose/', admin.site.urls),
     path('', include("YTdownloader.urls"))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler404 = 'YTdownloader.views.handler404'
 handler500 = 'YTdownloader.views.handler500'
