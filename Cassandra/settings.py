80712fe1199e51c62c049032e0ef0cb04cc0c143
rishav-jha-mech <rishav.rj74@gmail.com>
2021-10-21 00:12:47 +0530

    Set to true to check for errorrs

--- Cassandra/settings.py
+++ Cassandra/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-c7&e)$438!c*2h=gn=55eqi-55rmhokcy(5-28%)#hgt4%yaqt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','https://rj-cassandra-v2.herokuapp.com']
 
